42673bd58d45387c9e3ddbf48301588d10a9a778
김용민 <dydals3440@gmail.com>
2023-12-08 20:24:02 +0900

    feat: usePathname과 /explore 페이지

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,5 +1,26 @@
-import styles from './page.module.css';
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+import style from './explore.module.css';
 
 export default function Home() {
-  return <main className={styles.main}>검색페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
