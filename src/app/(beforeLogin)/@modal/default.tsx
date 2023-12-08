17260179b40589131a51f982a15f04afee718fd4
김용민 <dydals3440@gmail.com>
2023-12-09 01:36:31 +0900

    feat: 다중 이미지 구역 만들기

--- src/app/(beforeLogin)/@modal/default.tsx
+++ src/app/(beforeLogin)/@modal/default.tsx
@@ -5,3 +5,7 @@ export default function Default() {
 // 레이아웃 입장에서는
 // 주소가 z.com 일떄는 children -> page.tsx
 // modal -> @modal/default.tsx
+
+// 주소가 localhost:3001/i/flow/login 떄는
+// childrern -> i/flow/login/page.tsx
+// modal -> @modal/i/flow/login/page.tsx
