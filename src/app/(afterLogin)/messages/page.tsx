9bbe207ea6a188e56f1a37b082d22ed9bf2c201e
김용민 <dydals3440@gmail.com>
2023-12-08 23:37:58 +0900

    feat: messages 페이지 클론

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,5 +1,18 @@
-import styles from './page.module.css';
+import style from './message.module.css';
+import Room from '@/app/(afterLogin)/messages/_component/Room';
 
-export default function Message() {
-  return <main className={styles.main}>쪽지 페이지</main>;
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
