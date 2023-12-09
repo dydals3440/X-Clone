c420bc171db99e9849cfcab7678084e954670820
김용민 <dydals3440@gmail.com>
2023-12-09 17:38:29 +0900

    feat: MSW setting

---
+++ src/app/_component/MSWComponent.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { useEffect } from 'react';
+
+export const MSWComponent = () => {
+  useEffect(() => {
+    // 브라우저에서만 돌아간다는 것을 보장 MSW 2.0
+    if (typeof window !== 'undefined') {
+      if (process.env.NEXT_REPUBLIC_API_MOCKING === 'enabled') {
+        require('@/mocks/brower');
+      }
+    }
+  }, []);
+  return null;
+};
